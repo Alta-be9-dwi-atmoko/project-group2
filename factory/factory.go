e763825998b425a64a2953e37dc28aaecd2c6b4a
Dwi Atmoko <dwiatmokop@gmail.com>
2022-06-25 16:25:19 +0700

    added factory

--- factory/factory.go
+++ factory/factory.go
@@ -1,1 +1,24 @@
 package factory
+
+import (
+	_userBusiness "project/group2/features/users/business"
+	_userData "project/group2/features/users/data"
+	_userPresentation "project/group2/features/users/presentation"
+
+	"gorm.io/gorm"
+)
+
+type Presenter struct {
+	UserPresenter *_userPresentation.UserHandler
+}
+
+func InitFactory(dbConn *gorm.DB) Presenter {
+	// dbConn := config.InitDB()
+	userData := _userData.NewUserRepository(dbConn)
+	userBusiness := _userBusiness.NewUserBusiness(userData)
+	userPresentation := _userPresentation.NewUserHandler(userBusiness)
+
+	return Presenter{
+		UserPresenter: userPresentation,
+	}
+}
