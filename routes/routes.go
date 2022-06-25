a0913fb91fbad313ad7e265bc68979dc239c8a46
Dwi Atmoko <dwiatmokop@gmail.com>
2022-06-25 20:18:25 +0700

    added create user feature with bcrypted password

--- routes/routes.go
+++ routes/routes.go
@@ -11,5 +11,6 @@ func New(presenter factory.Presenter) *echo.Echo {
 	e := echo.New()
 
 	e.GET("/users", presenter.UserPresenter.GetAll)
+	e.POST("/users", presenter.UserPresenter.PostUser)
 	return e
 }
