2416f6ca834adbaafd77f5cfe669c607ecd675ac
Dwi Atmoko <dwiatmokop@gmail.com>
2022-06-25 18:55:31 +0700

    added jwt middleware

--- config/mysqldriver.go
+++ config/mysqldriver.go
@@ -24,7 +24,7 @@ func InitDB() *gorm.DB {
 		"DB_Name":     os.Getenv("DBname"),
 	}
 
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=UTC",
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		config["DB_Username"],
 		config["DB_Password"],
 		config["DB_Host"],
