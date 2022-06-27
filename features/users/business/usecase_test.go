71eec928e15829aeb8a6feacd07246853679f719
Dwi Atmoko <dwiatmokop@gmail.com>
2022-06-27 10:28:17 +0700

    added CORS

---
+++ features/users/business/usecase_test.go
@@ -0,0 +1,11 @@
+package business
+
+// import "testing"
+
+// type mockUserData struct{}
+
+// func TestGetAllData(t *testing.T) {
+// 	t.run("test get all data success", func(t *testing.T) {
+// 		userData := NewUserBusiness(mockUserData{})
+// 	})
+// }
--- routes/routes.go
+++ routes/routes.go
@@ -11,12 +11,18 @@ import (
 func New(presenter factory.Presenter) *echo.Echo {
 	// presenter := factory.InitFactory()
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH, echo.OPTIONS},
+	}))
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.GET("/users", presenter.UserPresenter.GetAll)
 	e.POST("/users", presenter.UserPresenter.PostUser)
 	e.POST("/login", presenter.UserPresenter.LoginAuth)
 	e.GET("/users/:id", presenter.UserPresenter.GetById, _middleware.JWTMiddleware())
-	e.PUT("/users/:id", presenter.UserPresenter.GetById, _middleware.JWTMiddleware())
+	e.PUT("/users/:id", presenter.UserPresenter.PutUser, _middleware.JWTMiddleware())
 	e.DELETE("/users/:id", presenter.UserPresenter.DeleteById, _middleware.JWTMiddleware())
 	return e
 }
