9b70baefb1e53ca0d201f08a70625e87c55f3cbd
Dwi Atmoko <dwiatmokop@gmail.com>
2022-06-29 09:00:50 +0700

    entity and gorm model for order feature

--- migration/migration.go
+++ migration/migration.go
@@ -2,6 +2,7 @@ package migration
 
 import (
 	_mCarts "project/group2/features/carts/data"
+	_mOrders "project/group2/features/orders/data"
 	_mProducts "project/group2/features/products/data"
 	_mUsers "project/group2/features/users/data"
 
@@ -12,4 +13,8 @@ func InitMigrate(db *gorm.DB) {
 	db.AutoMigrate(&_mUsers.User{})
 	db.AutoMigrate(&_mProducts.Product{})
 	db.AutoMigrate(&_mCarts.Cart{})
+	db.AutoMigrate(&_mOrders.Order{})
+	db.AutoMigrate(&_mOrders.OrderDetail{})
+	db.AutoMigrate(&_mOrders.Address{})
+	db.AutoMigrate(&_mOrders.Payment{})
 }
